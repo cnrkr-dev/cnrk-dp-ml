399888acea583947ca7bc1f14c85d72e0fb44f98
cnrkr-dev <cnrkr.dev@gmail.com>
2024-12-18 14:28:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/cnrkr-dev/data/master/penguins_cleaned.csv')
   df
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
