ebe13035f164144092921895c3d13edfacc2eb21
cnrkr-dev <cnrkr.dev@gmail.com>
2024-12-18 14:06:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App')
 
 st.write('This is a ml app!')
+
+df = pd.read_csv('https://github.com/cnrkr-dev/data/blob/main/penguins_cleaned.csv')
+df
