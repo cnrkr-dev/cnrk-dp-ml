6a167ea88a46a95dc1bfc60996bfc74fa5def673
cnrkr-dev <cnrkr.dev@gmail.com>
2024-12-18 14:25:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Machine Learning App')
 
 st.write('This is a ml app!')
 
-with st.expander('Data')
+with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/cnrkr-dev/data/master/penguins_cleaned.csv')
   df
